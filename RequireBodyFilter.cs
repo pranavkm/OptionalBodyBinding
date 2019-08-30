6dba8e97838da46017f442bde316d8f25ae0f945
Pranav K <prkrishn@hotmail.com>
2019-08-30 07:25:28 -0700

    Initial commit

---
+++ RequireBodyFilter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace OptionalBody
+{
+    public class RequireBodyFilter : ActionFilterAttribute
+    {
+        public RequireBodyFilter()
+        {
+            // Run before the ModelStateInvalidFilter
+            Order = -2001;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var controllerActionDescriptor = (ControllerActionDescriptor)context.ActionDescriptor;
+            var methodParameters = controllerActionDescriptor.MethodInfo.GetParameters();
+            for (var i = 0; i < context.ActionDescriptor.Parameters.Count; i++)
+            {
+                var parameter = context.ActionDescriptor.Parameters[i];
+                if (parameter.BindingInfo?.BindingSource != BindingSource.Body)
+                {
+                    continue;
+                }
+
+                if (methodParameters[i].HasDefaultValue)
+                {
+                    continue;
+                }
+
+                var boundValue = context.ActionArguments[parameter.Name];
+                if (boundValue == null)
+                {
+                    context.ModelState.AddModelError(parameter.Name, "Request body is required.");
+                    break;
+                }
+            }
+        }
+    }
+}
